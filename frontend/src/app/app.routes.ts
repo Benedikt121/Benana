6607e339b0f213ddef80fac5b19a7998a26b8dc6
Benedikt121 <bene.boeckle@icloud.com>
2025-10-19 15:27:16 +0200

    Added Login and register sites

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Oly } from './oly/oly';
 import { Kniffel } from './kniffel/kniffel';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
 
 export const routes: Routes = [
@@ -12,7 +14,9 @@ export const routes: Routes = [
 
     { path: 'kniffel', component: Kniffel},
 
+    { path: 'login', component: Login},
 
+    { path: 'register', component: Register},
 
     { path: '**', redirectTo: '' , pathMatch: 'full' }
 
