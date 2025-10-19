b95afd884094a878e9d89e5b5c301fdab9b0fb63
Benedikt121 <bene.boeckle@icloud.com>
2025-10-19 14:31:29 +0200

    Added heart-effect-function for touch

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -10,21 +10,39 @@ import { RouterLink } from '@angular/router';
 export class Home {
 constructor() { }
 
-@HostListener('document:mousemove', ['$event'])
+  @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
+    this.createHeart(e.clientX, e.clientY);
+  }
+
+  @HostListener('document:touchmove', ['$event'])
+  onTouchMove(e: TouchEvent) {
+    e.preventDefault(); 
+    
+    if (e.touches.length > 0) {
+      const touch = e.touches[0];
+      this.createHeart(touch.clientX, touch.clientY);
+    }
+  }
+
+  private createHeart(x: number, y: number) {
     const body = document.body;
     const heart = document.createElement('span');
-    const x = e.clientX;
-    const y = e.clientY;
-    heart.className = 'mouse-heart';
+    
     heart.style.left = x + 'px';
     heart.style.top = y + 'px';
+    
     const size = Math.random() * 40;
     heart.style.width = 20 + size + 'px';
     heart.style.height = 20 + size + 'px';
+    
     const transformValue = Math.random() * 360;
     heart.style.transform = `rotate(${transformValue}deg)`;
+    
     body.appendChild(heart);
-    setTimeout(() => heart.remove(), 1000);
+    
+    setTimeout(() => {
+      heart.remove();
+    }, 1000);
   }
 }
