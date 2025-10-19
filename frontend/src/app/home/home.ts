5c7cb2deef616636eaf7af6eb110281ade8a11b5
Benedikt121 <bene.boeckle@icloud.com>
2025-10-19 14:13:12 +0200

    Added Mouse-Heart effect (not workign yet)

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,5 +8,23 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home.css'
 })
 export class Home {
+constructor() { }
 
+@HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    const body = document.body;
+    const heart = document.createElement('span');
+    const x = e.clientX;
+    const y = e.clientY;
+    heart.className = 'mouse-heart';
+    heart.style.left = x + 'px';
+    heart.style.top = y + 'px';
+    const size = Math.random() * 80;
+    heart.style.width = 20 + size + 'px';
+    heart.style.height = 20 + size + 'px';
+    const transformValue = Math.random() * 360;
+    heart.style.transform = `rotate(${transformValue}deg)`;
+    body.appendChild(heart);
+    setTimeout(() => heart.remove(), 1000);
+  }
 }
